f5ca77e8c8f41d89be21bdf917b5e2d7d7b66802
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-05-13 00:06:10 +0900

    fix: delete unused module

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ClothesSelection from "./components/ClothesSelection";
 import CitiesSearch from "./components/CitiesSearch";
 
